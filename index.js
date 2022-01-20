ece1d8f83392467668ebf319aae8114fee924870
Lee Pavelich <leepavelich@gmail.com>
2022-01-20 01:49:02 +0000

    Implemented fetchCoordsByIP

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const fetchMyIP = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -7,3 +7,11 @@ const fetchMyIP = require('./iss');
 //   }
 //   console.log('It worked! Returned IP:' , ip);
 // });
+
+fetchCoordsByIP('104.205.246.244', (error, coord) => {
+  if (error) {
+    console.log('Error', error.message);
+    return;
+  }
+  console.log(coord);
+});
