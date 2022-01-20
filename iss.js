6e6ae3f7d29c051f1340bffc1c26ab95f7102b22
Lee Pavelich <leepavelich@gmail.com>
2022-01-20 02:18:39 +0000

    Variable name change

--- iss.js
+++ iss.js
@@ -73,9 +73,34 @@ const fetchISSFlyOverTimes = function(coords, callback) {
       callback(Error(msg), null);
       return;
     }
-    const risetimes = JSON.parse(body).response;
-    callback(null, risetimes);
+    const flyovers = JSON.parse(body).response;
+    callback(null, flyovers);
   });
 };
 
-module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
+/**
+ * Orchestrates multiple API requests in order to determine the next 5 upcoming ISS fly overs for the user's current location.
+ * Input:
+ *   - A callback with an error or results. 
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly-over times as an array (null if error):
+ *     [ { risetime: <number>, duration: <number> }, ... ]
+ */ 
+ const nextISSTimesForMyLocation = function(callback) {
+  fetchMyIP((error, ip) => {
+    if (error) return callback(error, null);
+
+    fetchCoordsByIP(ip, (error, coord) => {
+      if (error) return callback(error, null);
+
+      fetchISSFlyOverTimes(coord, (error, passes) => {
+        if (error) return callback(error, null);
+
+        callback(null, passes)
+      });
+    });
+  });
+}
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation };
