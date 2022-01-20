afed551b921b36cda5df05d7c698834c66c2965a
Lee Pavelich <leepavelich@gmail.com>
2022-01-20 01:15:03 +0000

    Added check for non-200 status codes

--- iss.js
+++ iss.js
@@ -8,10 +8,22 @@
  */
 const request = require('request');
 
- const fetchMyIP = callback => { 
+const fetchMyIP = callback => {
   request('https://api.ipify.org/?format=json', (error, response, body) => {
-    error ? callback(error, null) : callback(null, JSON.parse(body).ip)
+    // error can be set if invalid domain, user is offline, etc.
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    // if we get here, all's well and we got the data
+    callback(null, JSON.parse(body).ip);
   });
-}
+};
 
 module.exports = fetchMyIP;
