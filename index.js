e3c952eac4b7f97280a78ad60376b5eaf852b79c
Lee Pavelich <leepavelich@gmail.com>
2022-01-20 03:43:27 +0000

    Exported pass logging function

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const { nextISSTimesForMyLocation } = require('./iss');
 
-/**
- * Input:
+/** 
+ * Input: 
  *   Array of data objects defining the next fly-overs of the ISS.
  *   [ { risetime: <number>, duration: <number> }, ... ]
- * Returns:
+ * Returns: 
  *   undefined
- * Sideffect:
+ * Sideffect: 
  *   Console log messages to make that data more human readable.
  *   Example output:
  *   Next pass at Mon Jun 10 2019 20:11:44 GMT-0700 (Pacific Daylight Time) for 468 seconds!
@@ -14,11 +14,11 @@ const { nextISSTimesForMyLocation } = require('./iss');
 
 const logPasses = passTimes => {
   for (const pass of passTimes) {
-    const datetime = new Date.now();
+    const datetime = new Date(0);
     datetime.setUTCSeconds(pass.risetime);
-    console.log(`Next pass at ${datetime} for ${pass.duration} seconds!`);
+    console.log(`Next pass at ${datetime} for ${pass.duration} seconds!`)
   }
-};
+}
 
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
@@ -27,3 +27,5 @@ nextISSTimesForMyLocation((error, passTimes) => {
   // success, print out the deets!
   logPasses(passTimes);
 });
+
+module.exports = logPasses;
